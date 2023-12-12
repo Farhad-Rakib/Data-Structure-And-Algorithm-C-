f81ee3c256068cbb8f152671ce965d8cd0e7c251
Farhad Rakib <rakib.farhad@surbanajurong.com>
2023-12-12 13:10:37 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using DSAndAlgo.Sorting_Algorithm;
+using System;
+
+namespace DSAndAlgo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n;
+            Console.Write("Enter The Number Of Elements in the Array : ");
+
+            n = Convert.ToInt32(Console.ReadLine());
+            int[] unsortedArray = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                unsortedArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.Write("Unsorted Array is : ");
+            PrintArray(unsortedArray);
+            BubbleSortAlgorithm(unsortedArray, n);
+            Console.WriteLine();
+        }
+
+        public static void BubbleSortAlgorithm(int[] unsortedArray, int n)
+        {
+            Console.Write("\nSorted Array Using Bubble Sort Algorithm [Brute Froce Approach] : ");
+            PrintArray(BubbleSort.BubbleSortAlgorithmBruteForceApproach(unsortedArray, n));
+            Console.Write("\nSorted Array Using Bubble Sort Algorithm [Optimal Approach] : ");
+            PrintArray(BubbleSort.BubbleSortAlgorithmOptimalApproach(unsortedArray, n));
+        }
+
+        public static void PrintArray(int[] array1) 
+        {
+            for (int i = 0; i < array1.Length; i++)
+            {
+                Console.Write(array1[i] + " ");
+            }
+        }
+    }
+
+
+}
---
+++ Sorting Algorithm/BubbleSort.cs
@@ -0,0 +1,52 @@
+﻿namespace DSAndAlgo.Sorting_Algorithm
+{
+    public static class BubbleSort
+    {
+        public static int[] BubbleSortAlgorithmBruteForceApproach(int[] array1, int n)
+        {
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (array1[j] > array1[j + 1])
+                    {
+
+                        int temp = array1[j + 1];
+                        array1[j + 1] = array1[j];
+
+                        array1[j] = temp;
+
+                    }
+
+                }
+
+            }
+            return array1;
+        }
+
+        public static int[] BubbleSortAlgorithmOptimalApproach(int[] array1, int n)
+        {
+            for (int i = 0; i < n - 1; i++)
+            {
+                int flag = 0;
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (array1[j] > array1[j + 1])
+                    {
+
+                        int temp = array1[j + 1];
+                        array1[j + 1] = array1[j];
+
+                        array1[j] = temp;
+                        flag = 1;
+
+                    }
+
+                }
+                if (flag == 0) break;
+
+            }
+            return array1;
+        }
+    }
+}
